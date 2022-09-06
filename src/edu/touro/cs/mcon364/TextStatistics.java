1111679b0e1e9af2d4df256e5e5bcb149610ca4d
AharonK <aharon@ymkatz.net>
2022-09-06 13:01:50 -0400

    Added non-Stream answers

--- src/edu/touro/cs/mcon364/TextStatistics.java
+++ src/edu/touro/cs/mcon364/TextStatistics.java
@@ -1,5 +1,7 @@
 package edu.touro.cs.mcon364;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,11 +14,8 @@ public class TextStatistics {
      * @return the number of words in the input.
      */
     public static int wordCount(String text) {
-        //Pattern p = Pattern.compile("\\S+(\\s|$)");
-        //Matcher m = p.matcher(text);
-
-        Pattern p = Pattern.compile("\\S+\\s");
-        Matcher m = p.matcher(text+" ");
+        Pattern p = Pattern.compile("\\S+(\\s|$)");
+        Matcher m = p.matcher(text);
         return (int) m.results().count();
     }
 
@@ -26,11 +25,18 @@ public class TextStatistics {
      * @return the number of unique words in the input.
      */
     public static int uniqueWordCount(String text) {
-        // DRY
-        return wordStats(text).size();
+        Pattern p = Pattern.compile("\\S+(\\s|$)");
+        Matcher m = p.matcher(text);
 
-        // not DRY - case-sensitive
-        //return (int) Pattern.compile("\\S+(\\s|$)").matcher(text).results().distinct().count();
+        HashSet<String> uniqueWords = new HashSet<>();
+
+        while (m.find()) {
+            uniqueWords.add(m.group().toLowerCase().trim());
+        }
+        return uniqueWords.size();
+
+        // DRY option
+        // return wordStats(text).size();
     }
 
     /**
@@ -39,10 +45,24 @@ public class TextStatistics {
      * @return A mapping of the text with word as key and number of occurrences as value.
      */
     public static Map<String, Integer> wordStats(String text) {
+        Pattern p = Pattern.compile("\\S+(\\s|$)");
+        Matcher m = p.matcher(text);
+
+        HashMap<String, Integer> uniqueMap = new HashMap<>();
+
+        while (m.find()) {
+            String word = m.group().toLowerCase().trim();
+            uniqueMap.putIfAbsent(word, 0);
+            uniqueMap.put(word, uniqueMap.get(word)+1);
+        }
+        return uniqueMap;
+
+
+        // Using Streams
         // From https://stackoverflow.com/a/56974911
         // I feel like there's a more efficient way to do this, especially trim the results.
-        return Pattern.compile("(\\S+)(\\s|$)").matcher(text).results().collect(
-                Collectors.groupingBy(word -> word.group().toLowerCase().trim(), Collectors.summingInt(i -> 1)));
+        // return Pattern.compile("(\\S+)(\\s|$)").matcher(text).results().collect(
+        //        Collectors.groupingBy(word -> word.group().toLowerCase().trim(), Collectors.summingInt(i -> 1)));
     }
 
     /**
@@ -52,7 +72,19 @@ public class TextStatistics {
      * @return the count of each letter in the text.
      */
     public static int letterCount(String text) { // unique
-        return letterStats(text).size();
+        Pattern p = Pattern.compile("([a-zA-Z])");
+        Matcher m = p.matcher(text);
+
+        // Even though we're only looking for letters, there's no reason to convert them if we don't have to.
+        HashSet<String> uniqueWords = new HashSet<>();
+
+        while (m.find()) {
+            uniqueWords.add(m.group().toLowerCase());
+        }
+        return uniqueWords.size();
+
+        // DRY option
+        //return letterStats(text).size();
     }
 
     /**
@@ -61,7 +93,20 @@ public class TextStatistics {
      * @return A mapping of the text with letter as key and number of occurrences as value.
      */
     public static Map<Character, Integer> letterStats(String text) {
-        return Pattern.compile("([a-zA-Z])").matcher(text).results().collect(Collectors.groupingBy(
-                word -> word.group().toLowerCase().charAt(0), Collectors.summingInt(i -> 1)));
+        Pattern p = Pattern.compile("([a-zA-Z])");
+        Matcher m = p.matcher(text);
+
+        HashMap<Character, Integer> uniqueMap = new HashMap<>();
+
+        while (m.find()) {
+            char letter = m.group().toLowerCase().charAt(0);
+            uniqueMap.putIfAbsent(letter, 0);
+            uniqueMap.put(letter, uniqueMap.get(letter)+1);
+        }
+        return uniqueMap;
+
+        // Using Streams
+        //return Pattern.compile("([a-zA-Z])").matcher(text).results().collect(Collectors.groupingBy(
+        //        word -> word.group().toLowerCase().charAt(0), Collectors.summingInt(i -> 1)));
     }
 }
